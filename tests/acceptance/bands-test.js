fa77972c486a7e4b94ec81d01cf64d911d6f1fda
Dan Yip <Dan.Yip@3plearning.com>
2022-06-27 13:29:43 +1000

    tests:use data-test selectors + qunit-dom assert

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, click, fillIn, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'rarwe-octane/tests/helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { getPageTitle } from 'ember-page-title/test-support';
@@ -20,11 +20,39 @@ module('Acceptance | bands', function (hooks) {
       'Correct page title'
     );
 
-    const bandLinks = document.querySelectorAll('.mb-2 > a');
-    assert.strictEqual(bandLinks.length, 2, 'All band links are rendered');
-    assert.ok(
-      bandLinks[1].textContent.includes('Long Distance Calling'),
-      'The other band link contains the band name'
-    );
+    assert
+      .dom('[data-test-rr="band-link"]')
+      .exists({ count: 2 }, 'All band links are rendered');
+
+    assert
+      .dom('[data-test-rr="band-link"]')
+      .hasText('Radiohead', 'The first band link contains the band name');
+
+    assert
+      .dom('[data-test-rr="band-list-item"]:last-child')
+      .hasText(
+        'Long Distance Calling',
+        'The other band link contains the band name'
+      );
+  });
+
+  test('Create a band', async function (assert) {
+    this.server.create('band', { name: 'Royal Blood' });
+    await visit('/');
+    await click('[data-test-rr="new-band-button"]');
+    await fillIn('[data-test-rr="new-band-name"]', 'Caspian');
+    await click('[data-test-rr="save-band-button"]');
+    await waitFor('[data-test-rr="no-songs-text"]');
+
+    assert
+      .dom('[data-test-rr="band-list-item"]')
+      .exists({ count: 2 }, 'A new band link is rendered');
+
+    assert
+      .dom('[data-test-rr="band-list-item"]:last-child')
+      .hasText('Caspian', 'The new band link is rendered');
+    assert
+      .dom('[data-test-rr="songs-nav-item"] > .active')
+      .exists('The Songs tab is active');
   });
 });
